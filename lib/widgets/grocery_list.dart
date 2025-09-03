134aae1cf0392f384d5c954f97d0b3cbce31b06a
jzuber29 <zuberqa1996@gmail.com>
2025-09-03 18:42:11 +0530

    delete requests future builder widget

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,14 +1,13 @@
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:shopping_list/data/categories.dart';
 import 'package:shopping_list/models/category.dart';
-//import 'package:shopping_list/data/dummy_items.dart';
+// import 'package:shopping_list/data/dummy_items.dart';
 import 'package:shopping_list/models/grocery_item.dart';
 import 'package:shopping_list/widgets/new_item.dart';
 import 'package:http/http.dart' as http;
 
-class GroceryList extends StatefulWidget{
+class GroceryList extends StatefulWidget {
   const GroceryList({super.key});
 
   @override
@@ -16,122 +15,144 @@ class GroceryList extends StatefulWidget{
 }
 
 class _GroceryListState extends State<GroceryList> {
-   List<GroceryItem>_groceryItems=[];
-   var _isLoading =true;
-   String? _error;
+  List<GroceryItem> _groceryItems = [];
+  late Future<List<GroceryItem>> _loadedItems;
+  String? _error;
 
   @override
   void initState() {
     super.initState();
-    _loadItems();
+    _loadedItems = _loadItems();
   }
 
-  void  _loadItems()async {
-    final url=Uri.https(
-  'flutter-prep-64e86-default-rtdb.firebaseio.com','shopping-list.json');
-  final response=await http.get(url);
+  Future<List<GroceryItem>> _loadItems() async {
+    final url = Uri.https(
+      'flutter-prep-64e86-default-rtdb.firebaseio.com',
+      'shopping-list.json',
+    );
 
-  if (response.statusCode>=400){
-    setState(() {
-      _error='Failed to fetch data';
-    });
+    final response = await http.get(url);
+    if (response.statusCode >= 400) {
+      throw Exception('Failed to fetch grocery items');
+    }
 
-  }
+    if (response.body == 'null') {
+      return [];
+    }
 
-  print(response.statusCode);
- final Map<String, dynamic>listData=json.decode(response.body);
- final List <GroceryItem>loadedItems=[];
- for (final item in listData.entries){
-  final category=categories.entries.firstWhere((catItem)=>
-  catItem.value.title==item.value['category']).value;
-  loadedItems.add(GroceryItem(
-    id: item.key, name: item.value['name'], quantity: item.value['quantity'],
-     category: category));
+    final Map<String, dynamic> listData = json.decode(response.body);
+    final List<GroceryItem> loadedItems = [];
 
+    for (final item in listData.entries) {
+      final category = categories.entries
+          .firstWhere(
+            (catItem) => catItem.value.title == item.value['category'],
+          )
+          .value;
 
- }
-setState(() {
-  _groceryItems=loadedItems;
-  _isLoading=false;
-});
- 
-
-
-  }
-
-
-  void _addItem()async{
-    
-    final newItem=await Navigator.of(context).push<GroceryItem>(MaterialPageRoute(
-      builder: (ctx)=>NewItem()
-      ),
+      loadedItems.add(
+        GroceryItem(
+          id: item.key,
+          name: item.value['name'],
+          quantity: item.value['quantity'],
+          category: category,
+        ),
       );
-      if (newItem==null){
-        return ;
-      }
-      setState(() {
-        _groceryItems.add(newItem);
-      });
-       
-       
-       
-       }
+    }
+    return loadedItems;
+  }
 
-  void _removeItem(GroceryItem item){
+  void _addItem() async {
+    final newItem = await Navigator.of(context).push<GroceryItem>(
+      MaterialPageRoute(
+        builder: (ctx) => const NewItem(),
+      ),
+    );
+
+    if (newItem == null) {
+      return;
+    }
+
+    setState(() {
+      _groceryItems.add(newItem);
+    });
+  }
+
+  void _removeItem(GroceryItem item) async {
+    final index = _groceryItems.indexOf(item);
     setState(() {
       _groceryItems.remove(item);
     });
-   
-  }
 
+    final url = Uri.https(
+      'flutter-prep-64e86-default-rtdb.firebaseio.com',
+      'shopping-list/${item.id}.json',
+    );
+
+    final response = await http.delete(url);
+
+    if (response.statusCode >= 400) {
+      setState(() {
+        _groceryItems.insert(index, item);
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    Widget content=Center(child: Text('No items added yet'),);
-
-    if (_isLoading){
-      content=const Center(
-        child: CircularProgressIndicator(),
-
-      );
-    }
-    if (_groceryItems.isNotEmpty){
-      content= ListView.builder(
-        itemCount: _groceryItems.length,
-        itemBuilder: (ctx, index)=>
-      Dismissible(
-        onDismissed: (direction){
-         _removeItem(_groceryItems[index]);
-        },
-        key: ValueKey(_groceryItems[index].id),
-        child: ListTile(
-          title: Text(_groceryItems[index].name),
-          leading: Container(
-            width: 24,
-            height: 24,
-            color: _groceryItems[index].category.color,
-          ),
-          trailing: Text(_groceryItems[index].quantity.toString()),
-        ),
-      ),);
-    }
-
-    if (_error!=null){
-     content= Center(child: Text(_error!),);
-    }
-
-
     return Scaffold(
       appBar: AppBar(
-        title: Text('Your Groceries'),
+        title: const Text('Your Groceries'),
         actions: [
           IconButton(
-            onPressed: _addItem, icon: Icon (Icons.add),)
+            onPressed: _addItem,
+            icon: const Icon(Icons.add),
+          ),
         ],
       ),
-      body: content,
-      
+      body: FutureBuilder(
+        future: _loadedItems,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          if (snapshot.hasError) {
+            return Center(
+              child: Text(snapshot.error.toString()),
+            );
+          }
+
+          if (snapshot.data!.isEmpty) {
+            return const Center(
+              child: Text('No items added yet'),
+            );
+          }
+
+          return ListView.builder(
+            itemCount: snapshot.data!.length,
+            itemBuilder: (ctx, index) => Dismissible(
+              key: ValueKey(snapshot.data![index].id),
+              onDismissed: (direction) {
+                _removeItem(snapshot.data![index]);
+              },
+              child: ListTile(
+                title: Text(snapshot.data![index].name),
+                leading: Container(
+                  width: 24,
+                  height: 24,
+                  color: snapshot.data![index].category.color,
+                ),
+                trailing: Text(
+                  snapshot.data![index].quantity.toString(),
+                ),
+              ),
+            ),
+          );
+        },
+      ),
     );
-    
   }
 }
